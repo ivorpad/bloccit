8469eadff5d7920ab3bb0cbb356a367ba69ba14b
Ivor Padilla <ivorjpc@gmail.com>
2015-07-21 16:47:58 -0430

    Finished assignement 35

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,12 @@
 Rails.application.routes.draw do
-  resources :posts
-  get 'about' => 'welcome#about'
+  # get 'questions/index'
+  # get 'questions/show'
+  # get 'questions/new'
+  # get 'questions/edit'
 
+  resources :posts
+  resources :questions
+
+  get 'about' => 'welcome#about'
   root to: 'welcome#index'
 end
