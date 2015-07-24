9a6cd0400e9a810d295d6eca0ea3073dbf3fbce3
Ivor Padilla <ivorjpc@gmail.com>
2015-07-24 14:07:48 -0430

    Finished assignment 39

--- app/policies/post_policy.rb
+++ app/policies/post_policy.rb
@@ -1,5 +1,23 @@
 class PostPolicy < ApplicationPolicy
   def index?
-    true
+    user.present?
   end
+
+  class Scope < Scope
+    def resolve
+      if user.present?
+        if user.admin? || user.moderator?
+          scope.all
+        else
+          scope.where(published: true)
+        end
+      else
+        scope
+      end
+    end
+
+    def update?
+      user.admin? || !post.published?
+    end
+end
 end
