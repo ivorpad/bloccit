1d70aba1d46d56566f786ff67989d6f2908dcfd6
Ivor Padilla <ivorjpc@gmail.com>
2015-08-05 22:14:31 -0430

    Finished assignment 49

--- app/models/post.rb
+++ app/models/post.rb
@@ -13,6 +13,8 @@ class Post < ActiveRecord::Base
   # validates :topic, presence: true
   # validates :user, presence: true
 
+  after_create :create_vote
+
   def markdown_title
     render_as_markdown title
   end
@@ -36,11 +38,14 @@ class Post < ActiveRecord::Base
   def update_rank
     age_in_days = ( created_at - Time.new(1970, 1, 1) ) / ( 60 * 60 * 24 )
     new_rank = points + age_in_days
-
     update_attribute(:rank, new_rank)
   end
 
   private
+
+  def create_vote
+    user.votes.create(post: self, value: 1)
+  end
 
   def render_as_markdown(markdown)
     renderer = Redcarpet::Render::HTML.new
@@ -48,5 +53,6 @@ class Post < ActiveRecord::Base
     redcarpet =  Redcarpet::Markdown.new(renderer, extensions)
     (redcarpet.render markdown).html_safe
   end
+
 
 end
