df7ca8c5f771a6aec6e462974dcaac38a76dbf3f
Ivor Padilla <ivorjpc@gmail.com>
2015-08-25 23:21:24 -0430

    Fixed a merge conflict

--- spec/factories/user.rb
+++ spec/factories/user.rb
@@ -6,21 +6,11 @@
         password_confirmation "helloworld"
         confirmed_at Time.now
 
-<<<<<<< Updated upstream
-
-        factory :user_with_post_and_comment do
-            ## placeholder
-=======
         factory :user_with_post_and_comment do
           after(:build) do |user|
             post = create(:post, user: user)
             create(:comment, post: post, user: user)
           end
->>>>>>> Stashed changes
         end
-
       end
-
-
-
-  end
+    end
